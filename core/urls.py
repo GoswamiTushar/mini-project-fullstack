a0f7841b5cc3259cccaaa1d37ef2a0dd8ca3ce7d
Tushar Goswami <tushargoswami1510@gmail.com>
2021-11-26 22:28:39 +0530

    BUG: Updated urls.py file

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
-import settings
+from django.conf import settings
 from django.views.static import serve
-from django.conf.urls.static import url
+from django.conf.urls import url
 
 
 def index(request):
