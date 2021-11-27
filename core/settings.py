c7d9a632080326f7dbcbc11ed58e48588f0fd7b4
Tushar Goswami <tushargoswami1510@gmail.com>
2021-11-27 21:43:10 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -145,10 +145,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer",
-        "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
-        },
-        'ROUTING': 'chat.routing.channel_routing',
+#         "CONFIG": {
+#             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
+#         },
+#         'ROUTING': 'chat.routing.channel_routing',
     }
 }
 
